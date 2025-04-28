b79a6c19f4aa2b75e830588e402febaf732b7220
Lorenzo <lorenzo_ferron@hotmail.it>
2025-04-28 14:25:29 +0200

    Increase verbosity

--- .github/scripts/package_linux.sh
+++ .github/scripts/package_linux.sh
@@ -12,7 +12,7 @@ chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=linux-${VERSION_NAME}
 cp ../utilities/texstudio.svg appdir
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so -appimage
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -extra-plugins=iconengines/libqsvgicon.so -appimage -verbose=3
 # ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
 cp TeXstudio-${VERSION}-x86_64.AppImage ../texstudio-${VERSION}-x86_64.AppImage
 cp TeXstudio-${VERSION}-x86_64.AppImage ../texstudio-${GIT_VERSION}-x86_64.AppImage
